d3170dceee74979782d715b7b6827459825a9de5
2uwls <2uwls1126@gmail.com>
2025-07-14 00:26:30 +0900

    feat: CoordinateDto 생성

---
+++ src/main/java/com/gangchu/gangchutrip/route/dto/CoordinateDto.java
@@ -0,0 +1,11 @@
+package com.gangchu.gangchutrip.route.dto;
+
+import lombok.Data;
+
+@Data
+public class CoordinateDto {
+    private double x;
+    private double y;
+    private String name;
+    private Integer order;
+}
