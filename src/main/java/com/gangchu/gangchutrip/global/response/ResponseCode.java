624c13e3b72dfb3977d591501351c20e6d74f858
2uwls <2uwls1126@gmail.com>
2025-07-14 23:40:17 +0900

    refactor: 공통 ApiResponse 및 ResponseCode 적용하여 응답 포맷 통일
    # Conflicts:
    #	src/main/java/com/gangchu/gangchutrip/global/response/ResponseCode.java
    #	src/main/java/com/gangchu/gangchutrip/route/controller/RouteController.java
    #	src/main/java/com/gangchu/gangchutrip/route/service/RouteService.java

--- src/main/java/com/gangchu/gangchutrip/global/response/ResponseCode.java
+++ src/main/java/com/gangchu/gangchutrip/global/response/ResponseCode.java
@@ -15,8 +15,6 @@ public enum ResponseCode {
     NO_DATA(HttpStatus.NO_CONTENT, "NO DATA"),
     //400 validation
     OK(HttpStatus.OK, "OK"),
-    CREATED(HttpStatus.CREATED, "CREATED"),
-    //400 bad request
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "BAD REQUEST"),
     //ex
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "올바르지 않은 인자입니다"),
@@ -36,11 +34,7 @@ public enum ResponseCode {
 
     // RouteController 관련 응답 코드
     ROUTE_FOUND(HttpStatus.OK, "다중경유지 길찾기 성공"),
-    ROUTE_OPTIMIZED(HttpStatus.OK, "다중경유지 길찾기 최적화 성공"),
-    KAKAO_API_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "카카오 API 호출 오류"),
-    MISSING_ORIGIN(HttpStatus.BAD_REQUEST, "출발지는 필수입니다"),
-    MISSING_DESTINATION(HttpStatus.BAD_REQUEST, "목적지는 필수입니다"),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저 정보를 찾지 못했습니다");
+    ROUTE_OPTIMIZED(HttpStatus.OK, "다중경유지 길찾기 최적화 성공");
 
 
     private final HttpStatus status;
