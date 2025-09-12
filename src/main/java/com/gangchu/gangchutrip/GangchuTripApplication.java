586bbef41cbecec3e100a40ae6e9345696edc30f
gimogibumo <wolfbone0304@gmail.com>
2025-09-12 16:28:59 +0900

    fix: git branch resolved

--- src/main/java/com/gangchu/gangchutrip/GangchuTripApplication.java
+++ src/main/java/com/gangchu/gangchutrip/GangchuTripApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GangchuTripApplication {
 
+//    what the problem
     public static void main(String[] args) {
         SpringApplication.run(GangchuTripApplication.class, args);
     }
