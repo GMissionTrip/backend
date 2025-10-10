11d0c032df5be3d49d519521850f76211f83de30
leeedongjaee <harold306122@gmail.com>
2025-10-10 11:20:08 +0900

    아카이브 리포지토리 추가

--- src/main/java/com/gangchu/gangchutrip/archive/repository/ArchiveRepository.java
+++ src/main/java/com/gangchu/gangchutrip/archive/repository/ArchiveRepository.java
@@ -1,12 +1,21 @@
 package com.gangchu.gangchutrip.archive.repository;
 
 import com.gangchu.gangchutrip.global.entity.Archive;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-@Repository
+import java.util.List;
+
 public interface ArchiveRepository extends JpaRepository<Archive, Long> {
 
     List<Archive> findAllByMemberId(Long memberId);
+
+    @Query("SELECT a FROM Archive a WHERE a.memberId = :memberId")
+    List<Archive> findByMemberId(@Param("memberId") Long memberId);
+
+    @Query("SELECT a FROM Archive a WHERE a.travelId = :travelId")
+    List<Archive> findByTravelId(@Param("travelId") Long travelId);
+
+    boolean existsByMemberIdAndTravelId(Long memberId, Long travelId);
 }
